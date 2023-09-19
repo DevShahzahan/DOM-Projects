2fbb6c25e5a50715213b01c01af86bcfdfadf58f
ShahzahanIslam <md.shahzahanislam06@gmail.com>
2023-09-19 21:31:33 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+
+
+function sum(){
+    var num1 = parseInt(document.getElementById("input1").value);
+    var num2 = parseInt(document.getElementById("input2").value);
+    var result = num1 + num2 ;
+   document.getElementById("insert").value = result;
+}
